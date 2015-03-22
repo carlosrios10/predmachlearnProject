0cec62a9eb87b609245628bc6adfb46ee33fc253
Carlos <cm.rios.10@gmail.com>
2015-03-21 21:07:09 -0300

    deleting

--- preprocecing.R
+++
@@ -1,8 +0,0 @@
-# In this scrip we make a tydy data set
-# me quedo con aquellas variables que no tiene NA.
-# tambien elimino a aquellas que tienen que ver con el tiempo.
-resumen<-resumen(train)
-resumen<-resumen[resumen$na==0,]
-resumen<-resumen[resumen$vacio==0,]
-trainReducido<-train[,resumen$variable]
-trainReducido<-trainReducido[,-1:-7]
